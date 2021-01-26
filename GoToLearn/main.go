a2b9c4bc915e8c60ede7b3b7384dc5b64f713cfd
atlas <sunjon529@gmail.com>
2021-01-26 22:05:38 +0800

    inital

---
+++ GoToLearn/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("hello,你好，世界！！")
+}
---
+++ GoToLearn/models/student.go
@@ -0,0 +1,9 @@
+package models
+
+type Student struct{
+	ID int,					  //学号
+	Name string,			  //姓名
+	Grade int8,				  //年级
+	Class int8,               //班级
+	Speciality string,        //专业
+}
