0d8e04b1ccc6ceaae133db4282e0dc53e6c8a271
atlas <sunjon529@gmail.com>
2021-01-27 22:03:47 +0800

    change student

--- GoToLearn/models/student.go
+++ GoToLearn/models/student.go
@@ -3,6 +3,7 @@ package models
 type Student struct{
 	ID int,					  //学号
 	Name string,			  //姓名
+	Age int,				  //年龄
 	Grade int8,				  //年级
 	Class int8,               //班级
 	Speciality string,        //专业
